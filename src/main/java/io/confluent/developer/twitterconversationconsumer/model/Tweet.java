8a2bd1d49f103b8aa2e1d1ab9148066a13c6eb80
bhughes20 <bhughes567@gmail.com>
2022-11-03 14:01:50 +0000

    TCC-003 creating Tweet entity

---
+++ src/main/java/io/confluent/developer/twitterconversationconsumer/model/Tweet.java
@@ -0,0 +1,25 @@
+package io.confluent.developer.twitterconversationconsumer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "conversation_tweets")
+@Data
+@Builder
+@AllArgsConstructor
+public class Tweet {
+    @Id
+    private String id;
+    private String conversation_id;
+    private String created_at;
+    private String author_id;
+    private String text;
+    private boolean possibly_sensitive;
+}
--- src/test/java/io/confluent/developer/twitterconversationconsumer/service/TwitterServiceTest.java
+++ src/test/java/io/confluent/developer/twitterconversationconsumer/service/TwitterServiceTest.java
@@ -1,9 +1,30 @@
 package io.confluent.developer.twitterconversationconsumer.service;
 
+import io.confluent.developer.twitterconversationconsumer.model.Tweet;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ExtendWith(MockitoExtension.class)
 public class TwitterServiceTest {
+
+    @Test
+    public void givenTweetRecordObject_whenSaveTweet_thenShouldReturnTweetObject(){
+
+        Tweet tweet = Tweet.builder()
+                .id("1050118621198921728")
+                .conversation_id("1212092627178287104")
+                .created_at("2022-11-02T23:12:08.000Z")
+                .author_id("2244994945")
+                .text("This is sample twitter text")
+                .possibly_sensitive(false)
+                .build();
+
+        Tweet savedTweet = new Tweet();
+
+        assertThat(savedTweet).isNotNull();
+    }
 
 }
