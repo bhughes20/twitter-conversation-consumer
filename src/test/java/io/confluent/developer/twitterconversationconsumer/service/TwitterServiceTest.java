fc089bc86c9847ddb7c7b36cb354832a1d414715
bhughes20 <bhughes567@gmail.com>
2022-11-03 13:40:51 +0000

    TCC-002 create Twitter Service Test class

---
+++ src/test/java/io/confluent/developer/twitterconversationconsumer/service/TwitterServiceTest.java
@@ -0,0 +1,9 @@
+package io.confluent.developer.twitterconversationconsumer.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TwitterServiceTest {
+
+}
