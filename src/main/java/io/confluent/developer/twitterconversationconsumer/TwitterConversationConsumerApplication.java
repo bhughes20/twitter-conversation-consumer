f803a21256cacd59f532e137ec586d6f96d9ac8e
bhughes20 <bhughes567@gmail.com>
2022-11-03 13:37:02 +0000

    TCC-001 Initial project setup

---
+++ src/main/java/io/confluent/developer/twitterconversationconsumer/TwitterConversationConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.twitterconversationconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterConversationConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterConversationConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/confluent/developer/twitterconversationconsumer/TwitterConversationConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.twitterconversationconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterConversationConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
